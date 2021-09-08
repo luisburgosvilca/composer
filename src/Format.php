1cb416fd645a956b585f5f7a8601429a5fb1d118
Luis Burgos <luisburgosvilca@gmail.com>
2021-09-08 02:21:53 -0500

    strtolower

--- src/Format.php
+++ src/Format.php
@@ -6,6 +6,10 @@ class Format{
     public static function upperText($value){
         return strtoupper($value);
     }
+
+    public static function lowerText($value){
+        return strtolower($value);
+    }
 }
 
 ?>
